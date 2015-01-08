eba7af2ccf3848c81024cfa2b0f50bdd4b0890ec
Sanjay Menakuru <balasanjay@gmail.com>
2015-01-07 20:02:03 -0500

    added more useful tests

--- base/file_impl_test.cpp
+++ base/file_impl_test.cpp
@@ -24,7 +24,7 @@ TEST(StringFileTest, TestReadOutsideBounds) {
 
 TEST(DiskFileTest, Simple) {
   File* df = nullptr;
-  ASSERT_TRUE(DiskFile::LoadFile("./testdata/testfile.txt", &df));
+  ASSERT_TRUE(DiskFile::LoadFile("base/testdata/testfile.txt", &df));
   EXPECT_EQ(11, df->Size());
   ValidateFile(*df, 0, "test file\n");
   delete (DiskFile*)df;
