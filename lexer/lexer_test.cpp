ad5143111c7dc2d8d289b03a485bcce94468b136
nelk <nelkishere@gmail.com>
2015-01-08 19:40:25 -0500

    Lex Identifier Tests

--- lexer/lexer_test.cpp
+++ lexer/lexer_test.cpp
@@ -5,15 +5,16 @@ using base::FileSet;
 
 namespace lexer {
 
-
 class LexerTest : public ::testing::Test {
 protected:
+  LexerTest(): fs(nullptr) {}
+
   vector<vector<Token>> tokens;
   vector<Error> errors;
+  FileSet* fs;
 };
 
 TEST_F(LexerTest, EmptyFile) {
-  FileSet* fs = nullptr;
   ASSERT_TRUE(FileSet::Builder()
     .AddStringFile("foo.joos", "")
     .Build(&fs));
@@ -24,7 +25,6 @@ TEST_F(LexerTest, EmptyFile) {
 }
 
 TEST_F(LexerTest, Whitespace) {
-  FileSet* fs = nullptr;
   ASSERT_TRUE(FileSet::Builder()
     .AddStringFile("foo.joos", " \n    \r   \t")
     .Build(&fs));
@@ -39,7 +39,6 @@ TEST_F(LexerTest, Whitespace) {
 }
 
 TEST_F(LexerTest, Comment) {
-  FileSet* fs = nullptr;
   ASSERT_TRUE(FileSet::Builder()
     .AddStringFile("foo.joos", "// foo bar\n/*baz*/")
     .Build(&fs));
@@ -57,7 +56,6 @@ TEST_F(LexerTest, Comment) {
 }
 
 TEST_F(LexerTest, LineCommentAtEof) {
-  FileSet* fs = nullptr;
   ASSERT_TRUE(FileSet::Builder()
     .AddStringFile("foo.joos", "// foo bar")
     .Build(&fs));
@@ -71,6 +69,115 @@ TEST_F(LexerTest, LineCommentAtEof) {
   EXPECT_EQ(PosRange(0, 0, 10), tokens[0][0].pos);
 }
 
+TEST_F(LexerTest, SimpleInteger) {
+  ASSERT_TRUE(
+      FileSet::Builder()
+      .AddStringFile("foo.joos", "123")
+      .Build(&fs));
+
+  LexJoosFiles(fs, &tokens, &errors);
+  ASSERT_TRUE(errors.empty());
+  ASSERT_EQ(1, tokens.size());
+  ASSERT_EQ(1, tokens[0].size());
+  Token* t = &tokens[0][0];
+  EXPECT_EQ(INTEGER, t->type);
+  EXPECT_EQ(0, t->pos.begin.offset);
+  EXPECT_EQ(3, t->pos.end.offset);
+}
+
+TEST_F(LexerTest, LeadingZeroInteger) {
+  ASSERT_TRUE(
+      FileSet::Builder()
+      .AddStringFile("foo.joos", "023")
+      .Build(&fs));
+
+  ASSERT_ANY_THROW({
+    LexJoosFiles(fs, &tokens, &errors);
+  });
+}
+
+TEST_F(LexerTest, OnlyZero) {
+  ASSERT_TRUE(
+      FileSet::Builder()
+      .AddStringFile("foo.joos", "0")
+      .Build(&fs));
+
+  LexJoosFiles(fs, &tokens, &errors);
+  ASSERT_TRUE(errors.empty());
+  ASSERT_EQ(1, tokens.size());
+  ASSERT_EQ(1, tokens[0].size());
+  Token* t = &tokens[0][0];
+  EXPECT_EQ(INTEGER, t->type);
+  EXPECT_EQ(0, t->pos.begin.offset);
+  EXPECT_EQ(1, t->pos.end.offset);
+}
+
+TEST_F(LexerTest, SimpleIdentifier) {
+  ASSERT_TRUE(
+      FileSet::Builder()
+      .AddStringFile("foo.joos", "foo")
+      .Build(&fs));
+
+  LexJoosFiles(fs, &tokens, &errors);
+  ASSERT_TRUE(errors.empty());
+  ASSERT_EQ(1, tokens.size());
+  ASSERT_EQ(1, tokens[0].size());
+  Token* t = &tokens[0][0];
+  EXPECT_EQ(IDENTIFIER, t->type);
+  EXPECT_EQ(0, t->pos.begin.offset);
+  EXPECT_EQ(3, t->pos.end.offset);
+}
+
+TEST_F(LexerTest, NumberBeforeIdentifier) {
+  ASSERT_TRUE(
+      FileSet::Builder()
+      .AddStringFile("foo.joos", "3m")
+      .Build(&fs));
+
+  LexJoosFiles(fs, &tokens, &errors);
+  ASSERT_TRUE(errors.empty());
+  ASSERT_EQ(1, tokens.size());
+  ASSERT_EQ(2, tokens[0].size());
+
+  Token* t = &tokens[0][0];
+  EXPECT_EQ(INTEGER, t->type);
+  EXPECT_EQ(0, t->pos.begin.offset);
+  EXPECT_EQ(1, t->pos.end.offset);
+
+  t = &tokens[0][1];
+  EXPECT_EQ(IDENTIFIER, t->type);
+  EXPECT_EQ(1, t->pos.begin.offset);
+  EXPECT_EQ(2, t->pos.end.offset);
+}
+
+TEST_F(LexerTest, CommentBetweenIdentifiers) {
+  ASSERT_TRUE(
+      FileSet::Builder()
+      .AddStringFile("foo.joos", "abc/*foobar*/def")
+      .Build(&fs));
+
+  LexJoosFiles(fs, &tokens, &errors);
+  ASSERT_TRUE(errors.empty());
+  ASSERT_EQ(1, tokens.size());
+  ASSERT_EQ(3, tokens[0].size());
+
+  Token* t = &tokens[0][0];
+  EXPECT_EQ(IDENTIFIER, t->type);
+  EXPECT_EQ(0, t->pos.begin.offset);
+  EXPECT_EQ(3, t->pos.end.offset);
+
+  t = &tokens[0][1];
+  EXPECT_EQ(BLOCK_COMMENT, t->type);
+  EXPECT_EQ(3, t->pos.begin.offset);
+  EXPECT_EQ(13, t->pos.end.offset);
+
+  t = &tokens[0][2];
+  EXPECT_EQ(IDENTIFIER, t->type);
+  EXPECT_EQ(13, t->pos.begin.offset);
+  EXPECT_EQ(16, t->pos.end.offset);
+}
+
 // TODO: unclosed block comment test.
 
-}
+}  // namespace base
+
