372300ffb599bc16a0c42b563cddb97b8cb19267
nelk <nelkishere@gmail.com>
2015-02-10 23:23:26 -0500

    Example rewriter.

--- joosc.cpp
+++ joosc.cpp
@@ -6,6 +6,8 @@
 #include "parser/ast.h"
 #include "parser/print_visitor.h"
 #include "weeder/weeder.h"
+#include "typing/rewriter.h"
+#include "typing/fun_rewriter.h"
 #include <iostream>
 
 using base::ErrorList;
@@ -108,6 +110,10 @@ bool CompilerMain(CompilerStage stage, const vector<string>& files, ostream* out
   if (stage == CompilerStage::WEED) {
     return true;
   }
+
+  // TODO.
+  //parser::FunRewriter rewriter;
+  //program.reset(program.get()->Rewrite(&rewriter));
 
   // Print out the AST.
   {
---
+++ typing/fun_rewriter.h
@@ -0,0 +1,42 @@
+#ifndef TYPING_FUN_REWRITER_H
+#define TYPING_FUN_REWRITER_H
+
+#include "parser/ast.h"
+#include "typing/rewriter.h"
+
+namespace parser {
+
+class FunRewriter : public Rewriter {
+  REWRITE_VISIT_DECL(BoolLitExpr, Expr, expr);
+  REWRITE_VISIT_DECL(ClassDecl, TypeDecl, expr);
+};
+
+REWRITE_VISIT_DEFN(FunRewriter, BoolLitExpr, Expr, expr) {
+  lexer::Token tok = expr.GetToken();
+  if (tok.TypeInfo().Type() == lexer::K_TRUE) {
+    return new BoolLitExpr(lexer::Token(lexer::K_FALSE, tok.pos));
+  } else {
+    return new BoolLitExpr(lexer::Token(lexer::K_TRUE, tok.pos));
+  }
+}
+
+REWRITE_VISIT_DEFN(FunRewriter, ClassDecl, TypeDecl, type) {
+  ModifierList mods(type.Mods());
+  base::UniquePtrVector<ReferenceType> interfaces;
+  for (int i = 0; i < type.Interfaces().Size(); ++i) {
+    interfaces.Append(static_cast<ReferenceType*>(type.Interfaces().At(i)->clone()));
+  }
+  base::UniquePtrVector<MemberDecl> members;
+  for (int i = 0; i < type.Members().Size(); ++i) {
+    members.Append(type.Members().At(i)->Rewrite(this));
+  }
+  ReferenceType* super = nullptr;
+  if (type.Super() != nullptr) {
+    super = static_cast<ReferenceType*>(type.Super()->clone());
+  }
+  return new ClassDecl(std::move(mods), "Fail" + type.Name(), type.NameToken(), std::move(interfaces), std::move(members), super);
+}
+
+}
+
+#endif
