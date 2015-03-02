f938d2301470c6dcd02a689bed2631464401ff95
Sanjay Menakuru <balasanjay@gmail.com>
2015-03-01 22:28:43 -0500

    Added general infrastructure that can run any of a1-a4 tests.

--- z_a1_test.cpp
+++ z_a1_test.cpp
@@ -3,88 +3,198 @@
 #include <iostream>
 #include <iterator>
 
-#include "ast/ast.h"
-#include "base/error.h"
-#include "joosc.h"
-#include "lexer/lexer.h"
-#include "parser/parser.h"
 #include "third_party/gtest/gtest.h"
-#include "weeder/weeder.h"
 
-using std::back_inserter;
-using std::cerr;
-using std::copy_if;
-using std::cout;
+#include "base/error.h"
+#include "base/file_walker.h"
+#include "joosc.h"
 
-using ast::Program;
 using base::ErrorList;
 using base::FileSet;
-using base::PosRange;
-using lexer::FindUnsupportedTokens;
-using lexer::StripSkippableTokens;
-using lexer::Token;
-using parser::Parse;
 
 namespace {
 
-bool ListDir(const string& dirName, vector<string>* files) {
-  DIR* dir = opendir(dirName.c_str());
-  if (dir == nullptr) {
+bool IsJavaFile(const string& name) {
+  static const string kSuffix = ".java";
+  if (name.size() < kSuffix.size()) {
     return false;
   }
+  return name.compare(name.size() - kSuffix.size(), kSuffix.size(), kSuffix) == 0;
+}
 
-  while (true) {
-    struct dirent* ent = readdir(dir);
-    if (ent == nullptr) {
-      break;
+bool ListDirRecursive(const string& dir, vector<string>* out) {
+  auto cb = [&](const dirent& ent) {
+    string basename = string(ent.d_name);
+    string fullname = dir + '/' + basename;
+
+    // If its a regular file (i.e. Foo.java) then just append to out.
+    if (ent.d_type == DT_REG) {
+      out->push_back(fullname);
+      return true;
     }
-    if (ent->d_type != DT_REG) {
-      continue;
+
+    // If its a not a directory, then we don't care. It's probably a symlink or
+    // something weird.
+    if (ent.d_type != DT_DIR) {
+      return true;
     }
-    files->push_back(dirName + '/' + string(ent->d_name));
+
+    // If its either of the current directory or the parent directory, then
+    // bail to avoid infinite loops.
+    if (basename == "." || basename == "..") {
+      return true;
+    }
+
+    // Otherwise recurse.
+    ListDirRecursive(fullname, out);
+    return true;
+  };
+
+  return base::WalkDir(dir, cb);
+}
+
+struct CompileInput {
+  bool GetAllFiles(vector<string>* out) const;
+  std::ostream& operator<<(std::ostream& out) const;
+
+  // If not "", means a directory to search for .java files.
+  string stdlib_dir;
+
+  // The input file(s). Consult input_is_dir to know whether this is a file or
+  // directory name.
+  string input;
+  bool input_is_dir;
+
+  // The compile stage to stop at.
+  CompilerStage stage;
+};
+
+bool CompileInput::GetAllFiles(vector<string>* files) const {
+  vector<string> temp;
+  bool ok = true;
+  if (stdlib_dir != "") {
+    ok = ListDirRecursive(stdlib_dir, &temp) && ok;
   }
-  closedir(dir);
-  return true;
+
+  if (input_is_dir) {
+    ok = ListDirRecursive(input, &temp) && ok;
+  } else {
+    temp.push_back(input);
+  }
+
+  for (const auto& name : temp) {
+    if (IsJavaFile(name)) {
+      files->push_back(name);
+    }
+  }
+
+  return ok;
 }
 
-class CompilerSuccessTest : public testing::TestWithParam<string> {};
+// Print a CompileInput as a copy-pastable command-line incantation to run the
+// equivalent test.
+std::ostream& operator<<(std::ostream& out, const CompileInput& input) {
+  out << '"' << "find";
+  if (input.stdlib_dir != "") {
+    out << ' ' << input.stdlib_dir;
+  }
 
-TEST_P(CompilerSuccessTest, ShouldCompile) {
-  ASSERT_TRUE(CompilerMain(CompilerStage::WEED, {GetParam()}, &cout, &cerr));
+  out << ' ' << input.input << " -type f -name '*.java' | xargs ./joosc" << '"';
+  return out;
 }
 
-class CompilerFailureTest : public testing::TestWithParam<string> {};
+vector<CompileInput> GetInputs(const string& stdlib, const string& dir, CompilerStage stage, std::function<bool(const string&)> pred) {
+  vector<CompileInput> inputs;
 
-TEST_P(CompilerFailureTest, ShouldNotCompile) {
+  auto cb = [&](const dirent& ent) {
+    string basename = string(ent.d_name);
+    string fullname = dir + '/' + basename;
+
+    // If its a weird file, then bail.
+    if (ent.d_type != DT_REG && ent.d_type != DT_DIR) {
+      return true;
+    }
+
+    // If its either of the current directory or the parent directory, then
+    // bail to avoid infinite loops.
+    if (ent.d_type == DT_DIR && (basename == "." || basename == "..")) {
+      return true;
+    }
+
+    // If the client doesn't want it, then bail.
+    if (!pred(fullname)) {
+      return true;
+    }
+
+    // Otherwise, add a new CompileInput.
+    inputs.push_back({stdlib, fullname, ent.d_type == DT_DIR, stage});
+    return true;
+  };
+
+  assert(base::WalkDir(dir, cb));
+  return inputs;
+}
+
+vector<CompileInput> GetGoodInputs(const string& stdlib, const string& dir, CompilerStage stage) {
+  auto pred = [](const string& name) {
+    return name.find("Je") == string::npos;
+  };
+  return GetInputs(stdlib, dir, stage, pred);
+}
+
+vector<CompileInput> GetBadInputs(const string& stdlib, const string& dir, CompilerStage stage) {
+  auto pred = [](const string& name) {
+    return name.find("Je") != string::npos;
+  };
+  return GetInputs(stdlib, dir, stage, pred);
+}
+
+class CompilerSuccessTest2 : public testing::TestWithParam<CompileInput> {};
+
+TEST_P(CompilerSuccessTest2, ShouldCompile) {
+  vector<string> files;
+  ASSERT_TRUE(GetParam().GetAllFiles(&files));
+
   stringstream blackhole;
-  ASSERT_FALSE(CompilerMain(CompilerStage::WEED, {GetParam()}, &blackhole, &blackhole));
+  ASSERT_TRUE(CompilerMain(GetParam().stage, files, &blackhole, &blackhole));
 }
 
-vector<string> SuccessFiles() {
+class CompilerFailureTest2 : public testing::TestWithParam<CompileInput> {};
+
+TEST_P(CompilerFailureTest2, ShouldNotCompile) {
   vector<string> files;
-  assert(ListDir("third_party/cs444/assignment_testcases/a1", &files));
+  ASSERT_TRUE(GetParam().GetAllFiles(&files));
 
-  vector<string> filtered;
-  copy_if(files.begin(), files.end(), back_inserter(filtered),
-          [](const string& file) { return file.find("Je") == string::npos; });
-
-  return filtered;
+  stringstream blackhole;
+  ASSERT_FALSE(CompilerMain(GetParam().stage, files, &blackhole, &blackhole));
 }
 
-vector<string> FailureFiles() {
-  vector<string> files;
-  assert(ListDir("third_party/cs444/assignment_testcases/a1", &files));
+const static string kStdlib1 = "";
+const static string kStdlib2 = "third_party/cs444/stdlib/2.0";
+const static string kStdlib3 = "third_party/cs444/stdlib/3.0";
+const static string kStdlib4 = "third_party/cs444/stdlib/4.0";
+const static string kStdlib5 = "third_party/cs444/stdlib/5.0";
 
-  vector<string> filtered;
-  copy_if(files.begin(), files.end(), back_inserter(filtered),
-          [](const string& file) { return file.find("Je") != string::npos; });
+const static string kTest1 = "third_party/cs444/assignment_testcases/a1";
+const static string kTest2 = "third_party/cs444/assignment_testcases/a2";
+const static string kTest3 = "third_party/cs444/assignment_testcases/a3";
+const static string kTest4 = "third_party/cs444/assignment_testcases/a4";
+const static string kTest5 = "third_party/cs444/assignment_testcases/a5";
 
-  return filtered;
-}
+INSTANTIATE_TEST_CASE_P(MarmosetA1, CompilerSuccessTest2,
+    testing::ValuesIn(GetGoodInputs(
+        kStdlib1, kTest1, CompilerStage::WEED)));
 
-INSTANTIATE_TEST_CASE_P(MarmosetTests, CompilerSuccessTest,
-                        testing::ValuesIn(SuccessFiles()));
-INSTANTIATE_TEST_CASE_P(MarmosetTests, CompilerFailureTest,
-                        testing::ValuesIn(FailureFiles()));
+INSTANTIATE_TEST_CASE_P(MarmosetA1, CompilerFailureTest2,
+    testing::ValuesIn(GetBadInputs(
+        kStdlib1, kTest1, CompilerStage::WEED)));
+
+INSTANTIATE_TEST_CASE_P(MarmosetA2, CompilerSuccessTest2,
+    testing::ValuesIn(GetGoodInputs(
+        kStdlib2, kTest2, CompilerStage::TYPE_CHECK)));
+
+INSTANTIATE_TEST_CASE_P(MarmosetA2, CompilerFailureTest2,
+    testing::ValuesIn(GetBadInputs(
+        kStdlib2, kTest2, CompilerStage::TYPE_CHECK)));
 
 }  // namespace
