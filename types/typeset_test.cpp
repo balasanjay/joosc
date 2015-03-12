3371254c97ed720ee74d1c0df61b7b8569f4bbeb
jonathanwei <me@jonathanwei.ca>
2015-03-12 15:08:57 -0400

    Reformatted Typeset tests.

--- types/typeset_test.cpp
+++ types/typeset_test.cpp
@@ -5,25 +5,23 @@ namespace types {
 class TypeSetTest : public TypesTest {};
 
 TEST_F(TypeSetTest, TwoClassesWithSameQualifiedName) {
-  vector<pair<string, string>> test_files = {
+  ParseProgram({
     {"a/Foo.java", "package foo; public class Foo {}"},
     {"b/Foo.java", "package foo; public class Foo {}"},
-  };
-  ParseProgram(test_files);
+  });
   EXPECT_ERRS("TypeDuplicateDefinitionError: [0:26-29,1:26-29,]\n");
 }
 
 TEST_F(TypeSetTest, ClassAndPackageWithSameQualifiedName) {
-  vector<pair<string, string>> test_files = {
+  ParseProgram({
     {"a/Foo.java", "package foo.bar; public class Foo {}"},
     {"b/bar.java", "package foo; public class bar {}"},
-  };
-  ParseProgram(test_files);
+  });
   EXPECT_ERRS("TypeDuplicateDefinitionError: [1:26-29,0:12-15,]\n");
 }
 
 TEST_F(TypeSetTest, UnknownImport) {
-  vector<pair<string, string>> test_files = {
+  ParseProgram({
     {
       "a/Foo.java",
 
@@ -32,13 +30,12 @@ TEST_F(TypeSetTest, UnknownImport) {
       "  public Class y = null;\n"
       "}"
     },
-  };
-  ParseProgram(test_files);
+  });
   EXPECT_ERRS("UnknownImportError(0:7-20)\n");
 }
 
 TEST_F(TypeSetTest, MultipleWildcards) {
-  vector<pair<string, string>> test_files = {
+  ParseProgram({
     {"a/bar.java", "package a; public class bar {}"},
     {"b/bar.java", "package b; public class bar {}"},
     {"c/bar.java", "package c; public class bar {}"},
@@ -51,13 +48,12 @@ TEST_F(TypeSetTest, MultipleWildcards) {
       "import c.*;\n"
       "public class gee {}"
     },
-  };
-  ParseProgram(test_files);
+  });
   EXPECT_NO_ERRS();
 }
 
 TEST_F(TypeSetTest, WildcardsOverruledByPackage) {
-  vector<pair<string, string>> test_files = {
+  ParseProgram({
     {"a/bar.java", "package a; public class bar {}"},
     {"b/bar.java", "package b; public class bar {}"},
     {"c/bar.java", "package c; public class bar { public bar() {} }"},
@@ -69,13 +65,12 @@ TEST_F(TypeSetTest, WildcardsOverruledByPackage) {
       "import b.*;\n"
       "public class gee extends bar {}"
     },
-  };
-  ParseProgram(test_files);
+  });
   EXPECT_NO_ERRS();
 }
 
 TEST_F(TypeSetTest, WildcardsOverruledBySingleImport) {
-  vector<pair<string, string>> test_files = {
+  ParseProgram({
     {"a/bar.java", "package a; public class bar {}"},
     {"b/bar.java", "package b; public class bar {}"},
     {"c/bar.java", "package c; public class bar { public bar() {} }"},
@@ -88,13 +83,12 @@ TEST_F(TypeSetTest, WildcardsOverruledBySingleImport) {
       "import c.bar;\n"
       "public class gee extends bar {}"
     },
-  };
-  ParseProgram(test_files);
+  });
   EXPECT_NO_ERRS();
 }
 
 TEST_F(TypeSetTest, RedundantImport) {
-  vector<pair<string, string>> test_files = {
+  ParseProgram({
     {"a/bar.java", "package a; public class bar { public bar() {} }"},
     {
       "b/gee.java",
@@ -104,13 +98,12 @@ TEST_F(TypeSetTest, RedundantImport) {
       "import a.bar;\n"
       "public class gee extends bar {}"
     },
-  };
-  ParseProgram(test_files);
+  });
   EXPECT_NO_ERRS();
 }
 
 TEST_F(TypeSetTest, ConflictingImports) {
-  vector<pair<string, string>> test_files = {
+  ParseProgram({
     {"a/bar.java", "package a; public class bar {}"},
     {"b/bar.java", "package b; public class bar {}"},
     {
@@ -121,56 +114,50 @@ TEST_F(TypeSetTest, ConflictingImports) {
       "import b.bar;\n"
       "public class gee {}"
     },
-  };
-  ParseProgram(test_files);
+  });
   EXPECT_ERRS("DuplicateCompUnitNames: [2:32-37,2:18-23,]\n");
 }
 
 TEST_F(TypeSetTest, ConflictingImportAndType) {
-  vector<pair<string, string>> test_files = {
+  ParseProgram({
     {"a/bar.java", "package a; public class bar {}"},
     {"b/bar.java", "package b; import a.bar; public class bar {}"},
-  };
-  ParseProgram(test_files);
+  });
   EXPECT_ERRS("DuplicateCompUnitNames: [1:38-41,1:18-23,]\n");
 }
 
 TEST_F(TypeSetTest, UnknownQualifiedName) {
-  vector<pair<string, string>> test_files = {
+  ParseProgram({
     {"bar.java", "public class bar { public unknown.pkg.Class foo; }"},
-  };
-  ParseProgram(test_files);
+  });
   EXPECT_ERRS("UnknownTypenameError(0:26-43)\n");
 }
 
 TEST_F(TypeSetTest, QualifiedNameWithTypePrefix) {
-  vector<pair<string, string>> test_files = {
+  ParseProgram({
     {"foo/bar.java", "package foo; public class bar {}"},
     {"bar/baz.java", "package bar; public class baz {}"},
     {"test.java", "import foo.bar; public class test { public bar.baz field; }"},
-  };
-  ParseProgram(test_files);
+  });
   EXPECT_ERRS("TypeWithTypePrefixError(0:43-50)\n");
 }
 
 TEST_F(TypeSetTest, ShortNameNoMatch) {
-  vector<pair<string, string>> test_files = {
+  ParseProgram({
     {"test.java", "public class test { public Strng field; }"},
-  };
-  ParseProgram(test_files);
+  });
   EXPECT_ERRS("UnknownTypenameError(0:27-32)\n");
 }
 
 TEST_F(TypeSetTest, ShortNameExactMatch) {
-  vector<pair<string, string>> test_files = {
+  ParseProgram({
     {"test.java", "public class test { public String field; }"},
-  };
-  ParseProgram(test_files);
+  });
   EXPECT_NO_ERRS();
 }
 
 TEST_F(TypeSetTest, ShortNameMultipleIdenticalWildcards) {
-  vector<pair<string, string>> test_files = {
+  ParseProgram({
     {"foo/Foo.java", "package foo; public class Foo {}"},
     {
       "test.java",
@@ -181,13 +168,12 @@ TEST_F(TypeSetTest, ShortNameMultipleIdenticalWildcards) {
       "import foo.*;\n"
       "public class test { public Foo field; }"
     },
-  };
-  ParseProgram(test_files);
+  });
   EXPECT_NO_ERRS();
 }
 
 TEST_F(TypeSetTest, ShortNameMultipleAmbiguousWildcardsNoUse) {
-  vector<pair<string, string>> test_files = {
+  ParseProgram({
     {"foo/Foo.java", "package foo; public class Foo {}"},
     {"bar/Foo.java", "package bar; public class Foo {}"},
     {
@@ -197,13 +183,12 @@ TEST_F(TypeSetTest, ShortNameMultipleAmbiguousWildcardsNoUse) {
       "import bar.*;\n"
       "public class test {}"
     },
-  };
-  ParseProgram(test_files);
+  });
   EXPECT_NO_ERRS();
 }
 
 TEST_F(TypeSetTest, ShortNameMultipleAmbiguousWildcards) {
-  vector<pair<string, string>> test_files = {
+  ParseProgram({
     {"foo/Foo.java", "package foo; public class Foo {}"},
     {"bar/Foo.java", "package bar; public class Foo {}"},
     {
@@ -215,13 +200,12 @@ TEST_F(TypeSetTest, ShortNameMultipleAmbiguousWildcards) {
       "import bar.*;\n"
       "public class test { public Foo field; }"
     },
-  };
-  ParseProgram(test_files);
+  });
   EXPECT_ERRS("AmbiguousTypeError:[0:35-38,0:21-24,]\n");
 }
 
 TEST_F(TypeSetTest, ShortNameAmbiguousWildcardWithStdlib) {
-  vector<pair<string, string>> test_files = {
+  ParseProgram({
     {"bar/String.java", "package bar; public class String {}"},
     {
       "test.java",
@@ -229,17 +213,15 @@ TEST_F(TypeSetTest, ShortNameAmbiguousWildcardWithStdlib) {
       "import bar.*;\n"
       "public class test { public String field; }"
     },
-  };
-  ParseProgram(test_files);
+  });
   EXPECT_ERRS("AmbiguousTypeError:[0:7-10,-1:-1--1,]\n");
 }
 
 
 TEST_F(TypeSetTest, WildcardOfNonExistentPackage) {
-  vector<pair<string, string>> test_files = {
+  ParseProgram({
     {"test.java", "import non.existent.pkg.*;"},
-  };
-  ParseProgram(test_files);
+  });
   EXPECT_ERRS("UnknownPackageError(0:7-23)\n");
 }
 
