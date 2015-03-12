a32c4ff26ac0d7daec26b81d2a681d0f954827ce
jonathanwei <me@jonathanwei.ca>
2015-03-12 14:58:20 -0400

    Assign different primitive arrays test.

--- types/typechecker_hierarchy_test.cpp
+++ types/typechecker_hierarchy_test.cpp
@@ -201,6 +201,13 @@ TEST_F(TypeCheckerHierarchyTest, AssignArrayToAncestorArray) {
   EXPECT_NO_ERRS();
 }
 
+TEST_F(TypeCheckerHierarchyTest, AssignPrimitiveArrayToDifferentPrimitiveArray) {
+  TestSimpleHierarchy({
+    {"Main.java", "public class Main { public void foo() { int[] i = new byte[1]; return; } }"},
+  });
+  EXPECT_ERRS("UnassignableError(5:50-61)\n");
+}
+
 TEST_F(TypeCheckerHierarchyTest, AssignArrayToDescendantArray) {
   TestSimpleHierarchy({
     {"Main.java", "public class Main { public void foo() { D[] a = new A[1]; return; } }"},
