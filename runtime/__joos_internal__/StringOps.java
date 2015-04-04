387a301e10ecc03c989790819a0b187a59ad8585
Sanjay Menakuru <balasanjay@gmail.com>
2015-04-04 00:55:41 +0000

    Reduce allocatin in StringOps.Str.
    We originally thought that it needed to be a new String every time.
    
    However, we only use this in a concatenation context, and the
    String.concat will make a new String.

--- runtime/__joos_internal__/StringOps.java
+++ runtime/__joos_internal__/StringOps.java
@@ -2,18 +2,14 @@ R"(
 package __joos_internal__;
 
 public class StringOps {
-  public static String nullStr() {
-    return new String("null");
-  }
-
   public static String Str(Object o) {
     if (o == null) {
-      return StringOps.nullStr();
+      return "null";
     }
 
     String s = o.toString();
     if (s == null) {
-      return StringOps.nullStr();
+      return "null";
     }
 
     return s;
