bd51882b582162ded035c2373e00b9a254e53154
Sanjay Menakuru <balasanjay@gmail.com>
2015-09-25 00:26:04 -0700

    Remove a lot of nonsense from //tools.

--- tools/build_defs/scala/test/HelloLib.scala
+++
@@ -1,8 +0,0 @@
-package scala.test
-
-object HelloLib {
-  def printMessage(arg: String) {
-    println(arg + " " + OtherLib.getMessage())
-    println(arg + " " + OtherJavaLib.getMessage())
-  }
-}
